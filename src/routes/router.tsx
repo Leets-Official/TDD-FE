744dc36f75933ee7018acddbd0496ae1c83cf16c
wlsldm <lavesujin@gmail.com>
2026-07-15 23:43:59 +0900

    FEAT: notFoundPage 추가 및 라우트 연결

--- src/routes/router.tsx
+++ src/routes/router.tsx
@@ -1,10 +1,10 @@
 import { createBrowserRouter } from "react-router";
-
 import { RootLayout } from "@/layouts/RootLayout";
 import HomePage from "@/pages/home/HomePage";
 import LoginPage from "@/pages/login/LoginPage";
 import SignupPage from "@/pages/signup/SignupPage";
 import { PATH } from "@/routes/paths";
+import { NotFoundPage } from "@/pages/notFound/NotFoundPage";
 
 export const router = createBrowserRouter([
   {
@@ -13,6 +13,7 @@ export const router = createBrowserRouter([
       { path: PATH.HOME, element: <HomePage /> },
       { path: PATH.LOGIN, element: <LoginPage /> },
       { path: PATH.SIGNUP, element: <SignupPage /> },
+      { path: "*", element: <NotFoundPage /> },
     ],
   },
 ]);
