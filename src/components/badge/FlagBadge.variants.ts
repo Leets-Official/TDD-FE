d5960f07b8607958243e12ebfb94ea08cb041963
dongbi73 <ehdql0718@gachon.ac.kr>
2026-07-16 01:39:24 +0900

    FIX: FlagBadge 스타일 수정

--- src/components/badge/FlagBadge.variants.ts
+++ src/components/badge/FlagBadge.variants.ts
@@ -2,10 +2,10 @@ import { tv } from "@/utils/cn";
 
 export const flagBadgeVariants = tv({
   base: [
-    "relative inline-flex shrink-0 items-center whitespace-nowrap",
-    "bg-secondary py-[2px] pr-2 pl-[7px]",
+    "relative inline-flex h-5 shrink-0 items-center rounded-xs whitespace-nowrap",
+    "bg-secondary px-padding-xxs",
     "text-caption-1 text-text-1-color",
-    "before:absolute before:top-[5px] before:left-[-6px] before:h-0 before:w-0 before:content-['']",
+    "before:absolute before:top-[5px] before:left-[-5px] before:h-0 before:w-0 before:content-['']",
     "before:border-t-[5px] before:border-t-transparent",
     "before:border-b-[5px] before:border-b-transparent",
     "before:border-r-[6px] before:border-r-secondary",
