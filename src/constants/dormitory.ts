7ec30cf4d3091433eab27dd4237e92a4195845b0
wlsldm <lavesujin@gmail.com>
2026-07-22 15:02:04 +0900

    MOD: 기숙사 동 목록 공용 상수로 분리

---
+++ src/constants/dormitory.ts
@@ -0,0 +1,7 @@
+import type { DropdownOption } from "@/components/dropdown/Dropdown";
+
+export const DORMITORY_OPTIONS: DropdownOption[] = [
+  { label: "1기숙사", value: "1기숙사" },
+  { label: "2기숙사", value: "2기숙사" },
+  { label: "3기숙사", value: "3기숙사" },
+];
