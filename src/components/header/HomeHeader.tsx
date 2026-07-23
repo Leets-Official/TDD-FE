48f6970d7771abde301fc2719d5b96099f9d1bc0
wlsldm <lavesujin@gmail.com>
2026-07-23 16:09:00 +0900

    MOD: 헤더 디자인 변경 사항 반영 및 마이페이지 연결

--- src/components/header/HomeHeader.tsx
+++ src/components/header/HomeHeader.tsx
@@ -4,13 +4,16 @@ import { cn } from "@/utils/cn";
 
 import Logo from "@/assets/Logo.svg?react";
 import BoardIcon from "@/assets/icons/BoardIcon.svg?react";
-import MypageIcon from "@/assets/icons/MypageIcon.svg?react";
+import MypageIcon from "@/assets/icons/ProfileIcon.svg?react";
 
 import { IconButton } from "@/components/iconButton/IconButton";
+import { useNavigate } from "react-router";
+import { PATH } from "@/routes/paths";
 
 export type HomeHeaderProps = ComponentPropsWithRef<"header">;
 
 export function HomeHeader({ className, ...props }: HomeHeaderProps) {
+  const navigate = useNavigate();
   return (
     <header
       className={cn(
@@ -20,12 +23,27 @@ export function HomeHeader({ className, ...props }: HomeHeaderProps) {
       {...props}
     >
       <Logo aria-label="로고" className="ml-4 shrink-0" />
-      <div className="flex">
-        <IconButton aria-label="게시판" icon={<BoardIcon />} size="large" />
+      <div className="flex gap-2">
+        <IconButton
+          aria-label="게시판"
+          icon={
+            <div className="flex flex-col items-center">
+              <BoardIcon />
+              <span className="text-caption-2 text-black">게시판</span>
+            </div>
+          }
+          size="large"
+        />
         <IconButton
           aria-label="마이페이지"
-          icon={<MypageIcon />}
+          icon={
+            <div className="flex flex-col items-center">
+              <MypageIcon />
+              <span className="text-caption-2 text-black">마이페이지</span>
+            </div>
+          }
           size="large"
+          onClick={() => navigate(PATH.MYPAGE)}
         />
       </div>
     </header>
