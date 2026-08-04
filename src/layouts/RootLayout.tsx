d8594d83919eb05e9fd4cda1a50769a950ded9c5
wlsldm <lavesujin@gmail.com>
2026-08-04 17:29:59 +0900

    FIX: 핀치 확대를 허용하고 더블탭 줌만 차단하도록 수정

--- src/layouts/RootLayout.tsx
+++ src/layouts/RootLayout.tsx
@@ -1,11 +1,9 @@
 import { Outlet } from "react-router";
 
 import { useKeyboardInset } from "@/hooks/useKeyboardInset";
-import { usePreventPinchZoom } from "@/hooks/usePreventPinchZoom";
 
 export function RootLayout() {
   useKeyboardInset();
-  usePreventPinchZoom();
 
   return (
     <div className="mx-auto flex h-dvh w-full max-w-107.5 min-w-80 flex-col bg-white shadow-md">
